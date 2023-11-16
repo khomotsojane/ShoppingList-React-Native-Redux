0ad6346d52fa6f7413bb4edd1258d045cacfa8b3
Khomotso Sekhaolelo <111143532+khomotsojane@users.noreply.github.com>
2023-11-16 15:15:23 +0200

    Update YourCart.js

--- components/YourCart.js
+++ components/YourCart.js
@@ -3,17 +3,7 @@ import { View, Text, FlatList, Image, StyleSheet, TouchableOpacity } from "react
 
 export default function YourCart({ route }) {
   const { selectedItems } = route.params;
-  const [totalPrice, setTotalPrice] = useState(calculateTotalPrice(selectedItems));
-
-  const calculateTotalPrice = (items) => {
-    return items.reduce((acc, item) => acc + parseFloat(item.price), 0).toFixed(2);
-  };
-
-  const handleDeleteItem = (itemId) => {
-    const updatedItems = selectedItems.filter((item) => item.id !== itemId);
-    setSelectedItems(updatedItems);
-    setTotalPrice(calculateTotalPrice(updatedItems));
-  };
+  const [totalprice, setPrice] = useState(0);
 
   return (
     <View style={styles.container}>
@@ -21,25 +11,25 @@ export default function YourCart({ route }) {
       <FlatList
         data={selectedItems}
         renderItem={({ item }) => (
-          <TouchableOpacity onPress={() => handleDeleteItem(item.id)}>
-            <View style={styles.cartItemContainer}>
-              <Image source={item.src} style={styles.cartItemImage} />
-              <Text style={{ fontWeight: "bold" }}>{item.name}</Text>
-              <Text>{`Price R: ${item.price}`}</Text>
-              <Text>{`Brand: ${item.brand}`}</Text>
-            </View>
-          </TouchableOpacity>
+          <View style={styles.cartItemContainer}>
+            <Image source={item.src} style={styles.cartItemImage} />
+            <Text style={{ fontWeight: "bold" }}>{item.name}</Text>
+            <Text>{`Price R: ${item.price}`}</Text>
+            <Text>{`Brand: ${item.brand}`}</Text>
+          </View>
         )}
         keyExtractor={(item) => item.id.toString()}
       />
-      <Text>Total Price: {totalPrice}</Text>
+      <TouchableOpacity>
+        <Text>Price {selectedItems.reduce(function (acc, obj) { return parseFloat(acc) + parseFloat(obj.price) }, 0).toFixed(2)}</Text>
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    marginTop: 20,
+    marginTop:20,
     flex: 1,
     backgroundColor: "#fff",
     alignItems: "center",
@@ -49,8 +39,11 @@ const styles = StyleSheet.create({
     backgroundColor: "#DDD5F3",
     width: 300,
     height: 200,
+    //marginHorizontal: 20,
     marginVertical: 10,
     borderRadius: 10,
+    //alignContent: "center",
+    //alignItems: "center",
   },
   cartItemImage: {
     borderRadius: 10,
